5464ec1dab1bcab32f346b7aba8d98c5fe99231b
Nguyễn Đức Luận <ducluan05102001@gmail.com>
2026-06-23 14:24:39 +0700

    fix: adjust formatting in next.config.ts for consistency

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   // // Tạo output standalone để Docker image nhỏ hơn (không cần node_modules)
-  // output: "standalone",
+  // output: "standalone", 
 };
 
 export default nextConfig;
