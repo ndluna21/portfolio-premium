60c7ae3022bf55ae8ffa82bda13bc3a8ef4d3687
Nguyễn Đức Luận <ducluan05102001@gmail.com>
2026-06-23 13:30:55 +0700

    update

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,8 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  // // Tạo output standalone để Docker image nhỏ hơn (không cần node_modules)
+  // output: "standalone",
 };
 
 export default nextConfig;
